be8c52cb3cf547f77b2cf23a629182b6f2c2c446
spape <github@pape.io>
2014-10-09 16:36:57 +0200

    refactor cleanup services

--- spec/support/cleanup_services.rb
+++ spec/support/cleanup_services.rb
@@ -12,7 +12,7 @@ end
 
 RSpec.configure do |config|
 
-  config.after(:each) do
+  config.before(:each) do
     LHS::Service::CHILDREN.each do |child|
       child.instance.endpoints = [] if !child.name['LHS']
     end
