8fe5ff4187f0c70421a665a8d8850bf9d7bce537
spape <github@pape.io>
2014-11-24 16:14:22 +0100

    ! refactor due to feedback

--- lib/lhs/concerns/item/save.rb
+++ lib/lhs/concerns/item/save.rb
@@ -6,31 +6,20 @@ class LHS::Item < LHS::Proxy
     extend ActiveSupport::Concern
 
     def save
-      _save_
+      save!
       rescue LHC::Error => e
         self.errors = LHS::Errors.new(e.response)
         false
     end
 
     def save!
-      _save_
-    end
-
-    private
-
-    def _save_
       service = _data_._root_._service_
       data = _data_._raw_.dup
-      url = _url_(service, data)
+      url = href if href
+      url ||= service.instance.find_endpoint(params).compile(params)
       response = service.instance.request(method: :post, url: url, body: data.to_json, headers: {'Content-Type' => 'application/json'})
       self._data_.merge!(response)
       true
-    end
-
-    def _url_(service, params = {})
-      return href if href
-      endpoint = service.instance.find_endpoint(params)
-      endpoint.compile(params)
     end
   end
 end
--- lib/lhs/concerns/item/update.rb
+++ lib/lhs/concerns/item/update.rb
@@ -6,19 +6,13 @@ class LHS::Item < LHS::Proxy
     extend ActiveSupport::Concern
 
     def update(params)
-      _update_(params)
+      update!(params)
     rescue LHC::Error => e
       self.errors = LHS::Errors.new(e.response)
       false
     end
 
     def update!(params)
-      _update_(params)
-    end
-
-    private
-
-    def _update_(params)
       service = _data_._root_._service_
       data = _data_._raw_.dup
       response = service.instance.request(method: :post, url: href, body: data.merge(params).to_json, headers: {'Content-Type' => 'application/json'})
