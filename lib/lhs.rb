526082bc0c8c337a9f1215b9c1547195ef8c7a7b
spape <github@pape.io>
2015-10-02 10:04:10 +0200

    ! also load files from subdirectories inside app/services

--- lib/lhs.rb
+++ lib/lhs.rb
@@ -8,6 +8,6 @@ Gem.find_files('lhs/**/*.rb').each { |path| require path }
 # Preload all the services that are defined in app/services
 class Engine < Rails::Engine
   initializer 'Load all services' do |app|
-    Dir.glob(app.root.join('app/services/**.rb')).each {|file| require file }
+    Dir.glob(app.root.join('app/services/**/*.rb')).each {|file| require file }
   end
 end
