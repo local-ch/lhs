6a4118ed6f37855dbf490d4b5eda5c3aba09f620
spape <github@pape.io>
2016-02-22 16:07:20 +0100

    pagination v2.2.0

--- lib/lhs/data.rb
+++ lib/lhs/data.rb
@@ -37,6 +37,33 @@ class LHS::Data
   def _raw=(raw)
     raw.to_hash.deep_symbolize_keys! if raw && raw.respond_to?(:to_hash)
     @_raw = raw
+
+  def current_page
+    offset + 1
+  end
+
+  def first_page
+    1
+  end
+
+  def last_page
+    total_pages
+  end
+
+  def prev_page
+    current_page - 1
+  end
+
+  def next_page
+    current_page + 1
+  end
+
+  def limit_value
+    limit
+  end
+
+  def total_pages
+    total / limit
   end
 
   protected
