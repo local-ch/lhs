713fc71e396d5ba7be40a2ba01168a4cfd7f8b84
spape <github@pape.io>
2016-02-22 16:07:20 +0100

    pagination v1.6.0

--- lib/lhs/data.rb
+++ lib/lhs/data.rb
@@ -33,6 +33,34 @@ class LHS::Data
     _root._service
   end
 
+  def current_page
+    offset + 1
+  end
+
+  def first_page
+    1
+  end
+
+  def last_page
+    total_pages
+  end
+
+  def prev_page
+    current_page - 1
+  end
+
+  def next_page
+    current_page + 1
+  end
+
+  def limit_value
+    limit
+  end
+
+  def total_pages
+    total / limit
+  end
+
   protected
 
   # Use existing mapping to provide data
--- lib/lhs/version.rb
+++ lib/lhs/version.rb
@@ -1,3 +1,3 @@
 module LHS
-  VERSION = "1.5.0"
+  VERSION = "1.6.0"
 end
