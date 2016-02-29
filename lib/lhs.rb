fddf177514596c1fe32fd335be1a8d97a2eb62e2
spape <github@pape.io>
2016-02-29 11:07:53 +0100

    load all LHS records from app/models/**

--- lib/lhs.rb
+++ lib/lhs.rb
@@ -7,8 +7,8 @@ Gem.find_files('lhs/**/*.rb').each { |path| require path }
 
 # Preload all the LHS::Records that are defined in app/models
 class Engine < Rails::Engine
-  initializer 'Load all LHS::Records' do |app|
-    Dir.glob(app.root.join('app/services/**/*.rb')).each do |file|
+  initializer 'Load all LHS::Records from app/models/**' do |app|
+    Dir.glob(app.root.join('app/models/**/*.rb')).each do |file|
       require file if File.read(file).scan('LHS::Record')
     end
   end
