3cbc8fbfa524b683c45125bfb888423b53d21555
spape <github@pape.io>
2016-02-29 11:05:49 +0100

    ! lhs record autoloading for files in models

--- lib/lhs.rb
+++ lib/lhs.rb
@@ -5,9 +5,11 @@ end
 
 Gem.find_files('lhs/**/*.rb').each { |path| require path }
 
-# Preload all the services that are defined in app/services
+# Preload all the LHS::Records that are defined in app/models
 class Engine < Rails::Engine
-  initializer 'Load all services' do |app|
-    Dir.glob(app.root.join('app/services/**/*.rb')).each {|file| require file }
+  initializer 'Load all LHS::Records' do |app|
+    Dir.glob(app.root.join('app/services/**/*.rb')).each do |file|
+      require file if File.read(file).scan('LHS::Record')
+    end
   end
 end
