a8009dc907d73d6626c560d7c158611e81cae29b
Krzysztof Sakwerda <ksakwerda@gmail.com>
2016-03-01 17:25:22 +0100

    + remove data used to calculate url on save

--- lib/lhs/concerns/item/save.rb
+++ lib/lhs/concerns/item/save.rb
@@ -16,12 +16,14 @@ class LHS::Item < LHS::Proxy
     def save!
       record = _data._root._record_class
       data = _data._raw.dup
-      url =
-        if href.present?
-          href
-        else
-          record.find_endpoint(data).compile(data)
-        end
+      if href.present?
+        url = href
+      else
+        endpoint = record.find_endpoint(data)
+        url = endpoint.compile(data)
+        endpoint.remove_interpolated_params!(data)
+      end
+
       data = record.request(method: :post, url: url, body: data.to_json, headers: { 'Content-Type' => 'application/json' })
       _data.merge_raw!(data)
       true
