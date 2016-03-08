0d038c73f478d65bf0dd429e5fc8b897069db998
spape <github@pape.io>
2016-03-08 09:40:28 +0100

    ~ make constant out of magic number

--- lib/lhs/concerns/record/all.rb
+++ lib/lhs/concerns/record/all.rb
@@ -5,6 +5,8 @@ class LHS::Record
   module All
     extend ActiveSupport::Concern
 
+    DEFAULT_LIMIT = 100
+
     module ClassMethods
       # Should be an edge case but sometimes all objects from a certain resource
       # are required. In this case we load the first page with the default max limit,
@@ -12,7 +14,7 @@ class LHS::Record
       # for the following pages and concatenate all the results in order to return
       # all the objects for a given resource.
       def all(params = {})
-        limit = params[limit_key] || 100
+        limit = params[limit_key] || DEFAULT_LIMIT
         data = request(params: params.merge(limit_key => limit))
         request_all_the_rest(data, params) if data._raw.is_a?(Hash) && data._raw[total_key]
         data._record.new(LHS::Data.new(data, nil, self))
