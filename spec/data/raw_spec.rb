cb7aee905467d2703364a270461ce990fc48d152
Franca Rast <franca.rast@local.ch>
2016-03-17 12:01:28 +0100

    Revert "refactor context"
    This reverts commit b9bf43eae5a715ad2343a969cc23176fcf25222d.

--- spec/data/raw_spec.rb
+++ spec/data/raw_spec.rb
@@ -9,33 +9,26 @@ describe LHS::Data do
     end
   end
 
+  let(:data_from_raw) do
+    LHS::Data.new({ href: 'http://www.local.ch/v2/stuff', id: '123123123' }, nil, Record)
+  end
+
+  let(:data_from_item) do
+    raw = { href: 'http://www.local.ch/v2/stuff' }
+    item = LHS::Item.new(LHS::Data.new(raw, nil, Record))
+    LHS::Data.new(item)
+  end
 
   context 'raw' do
-    let(:data) do
-      LHS::Data.new({ href: 'http://www.local.ch/v2/stuff', id: '123123123' }, nil, Record)
-    end
-
     it 'you can access raw data that is underlying' do
-      expect(data._raw).to be_kind_of Hash
+      expect(data_from_raw._raw).to be_kind_of Hash
     end
 
-    it 'returns a hash with symoblized keys' do
-      expect(data._raw.keys.first).to be_kind_of Symbol
-    end
-
-    context 'data_from_item' do
-      let(:data) do
-        raw = { href: 'http://www.local.ch/v2/stuff' }
-        item = LHS::Item.new(LHS::Data.new(raw, nil, Record))
-        LHS::Data.new(item)
-      end
-
-      it 'forwards raw when you feed data with some LHS object' do
-        expect(data._raw).to be_kind_of Hash
-        expect(data._raw).to eq(
-          href: 'http://www.local.ch/v2/stuff'
-        )
-      end
+    it 'forwards raw when you feed data with some LHS object' do
+      expect(data_from_item._raw).to be_kind_of Hash
+      expect(data_from_item._raw).to eq(
+        href: 'http://www.local.ch/v2/stuff'
+      )
     end
   end
 end
