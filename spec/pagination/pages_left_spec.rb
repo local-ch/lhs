acb3c4d9a274f64195c44ff891d693238b418273
Franca Rast <franca.rast@local.ch>
2016-04-19 16:56:35 +0200

    set default for nil offset

---
+++ spec/pagination/pages_left_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe LHS::Record do
+  let(:offset) { 0 }
+  let(:data_hash) { { items: 98.times.map { { foo: 'bar' } }, total: 98, offset: offset, limit: 10 } }
+
+  let(:data) do
+    LHS::Data.new(data_hash, nil, Record)
+  end
+
+  let(:pagination) { OffsetPagination.new(data) }
+
+  before(:each) do
+    class Record < LHS::Record
+      endpoint ':datastore/v2/data'
+    end
+  end
+
+  it 'responds to pages_left' do
+    expect(pagination.pages_left).to eq(9)
+  end
+
+  context 'when there is no offset' do
+    let(:offset) { nil }
+
+    it 'responds to pages_left' do
+      # TODO i now set the nil offset to zero. Is this ok or wrong?
+      expect(pagination.pages_left).to eq(9)
+    end
+  end
+end
