6cf2c2f4d9c17bb0dd4c85c92be57f58f6953666
spape <github@pape.io>
2016-05-04 14:21:10 +0200

    + complete tests

--- spec/record/where_chains_spec.rb
+++ spec/record/where_chains_spec.rb
@@ -5,6 +5,10 @@ describe LHS::Record do
     'http://datastore/v2'
   end
 
+  let(:response) do
+    { body: [{ name: 'Steve' }] }
+  end
+
   before(:each) do
     LHC.config.placeholder('datastore', datastore)
     class Record < LHS::Record
@@ -21,24 +25,35 @@ describe LHS::Record do
   end
 
   context 'where chains' do
-    it 'allows chaining where statements' do
+    before(:each) do
       stub_request(:get, "http://datastore/v2/records/?available=true&color=blue&range=%3E26")
-        .to_return(body: [{ name: 'Steve' }])
-      records = Record.blue.where(range: '>26')
-      records = records.where(available: true)
+        .to_return(response)
+    end
+
+    let(:records) { Record.blue.where(range: '>26').where(available: true) }
+
+    it 'allows chaining where statements' do
       expect(records.class).to eq Record
       expect(records._raw).to eq [{ name: 'Steve' }]
       expect(records.first.uppercase_name).to eq 'STEVE'
-      expect(records.first.name).to eq 'Steve'
+    end
+
+    it 'resolves triggered by method missing' do
+      expect(records._raw).to eq [{ name: 'Steve' }]
+      expect(
+        Record.blue.where(range: '>26', available: true).first.name
+      ).to eq 'Steve'
+    end
+  end
+
+  context 'multiple paramters' do
+    before(:each) do
+      stub_request(:get, "http://datastore/v2/records/?parameter=last").to_return(response)
     end
 
     it 'takes the last value for chains with same name parameters' do
-      stub_request(:get, "http://datastore/v2/records/?parameter=last")
-        .to_return(body: [{ name: 'Steve' }])
       records = Record.where(parameter: 'first').where(parameter: 'last')
-      # initiate the call, the stub will fail if the wrong parameter 'won'
       records.first
     end
-    # TODO: add a test that resolves to method_missing
   end
 end
