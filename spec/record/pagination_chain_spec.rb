9b7f18a226130902a7dbac12212added279c8455
spape <github@pape.io>
2016-05-24 16:16:08 +0200

    ! specs

--- spec/record/pagination_chain_spec.rb
+++ spec/record/pagination_chain_spec.rb
@@ -11,27 +11,32 @@ describe LHS::Record do
       end
 
       it 'allows to chain pagination methods' do
-        stub_request(:get, "http://local.ch/records?color=blue&offset=200&limit=100").to_return(body: [].to_json)
+        request = stub_request(:get, "http://local.ch/records?color=blue&offset=200&limit=100").to_return(body: [].to_json)
         Record.where(color: 'blue').page(3).first
-        stub_request(:get, "http://local.ch/records?color=blue&offset=20&limit=10").to_return(body: [].to_json)
+        expect(request).to have_been_made.times(1)
+        request = stub_request(:get, "http://local.ch/records?color=blue&offset=20&limit=10").to_return(body: [].to_json)
         Record.where(color: 'blue').page(3).per(10).first
         Record.where(color: 'blue').per(10).page(3).first
         Record.where(color: 'blue').per(20).page(5).per(10).page(3).first
+        expect(request).to have_been_made.times(3)
       end
 
       it 'allows to start chains with pagination methods' do
-        stub_request(:get, "http://local.ch/records?color=blue&offset=200&limit=100").to_return(body: [].to_json)
+        request = stub_request(:get, "http://local.ch/records?color=blue&offset=200&limit=100").to_return(body: [].to_json)
         Record.page(3).where(color: 'blue').first
-        stub_request(:get, "http://local.ch/records?color=blue&offset=20&limit=10").to_return(body: [].to_json)
+        expect(request).to have_been_made.times(1)
+        request = stub_request(:get, "http://local.ch/records?color=blue&offset=20&limit=10").to_return(body: [].to_json)
         Record.page(3).per(10).where(color: 'blue').first
         Record.per(10).page(3).where(color: 'blue').first
         Record.per(20).page(5).where(color: 'blue').per(10).page(3).first
+        expect(request).to have_been_made.times(3)
       end
 
       it 'defaults page to 1' do
-        stub_request(:get, "http://local.ch/records?limit=10&offset=0").to_return(body: [].to_json)
+        request = stub_request(:get, "http://local.ch/records?limit=10&offset=0").to_return(body: [].to_json)
         Record.per(10).first
         Record.per(10).page("").first
+        expect(request).to have_been_made.times(2)
       end
     end
 
@@ -45,12 +50,14 @@ describe LHS::Record do
       end
 
       it 'allows to chain pagination methods' do
-        stub_request(:get, "http://local.ch/records?color=blue&start=201&limit=100").to_return(body: [].to_json)
+        request = stub_request(:get, "http://local.ch/records?color=blue&start=201&limit=100").to_return(body: [].to_json)
         Record.where(color: 'blue').page(3).first
-        stub_request(:get, "http://local.ch/records?color=blue&start=21&limit=10").to_return(body: [].to_json)
+        expect(request).to have_been_made.times(1)
+        request = stub_request(:get, "http://local.ch/records?color=blue&start=21&limit=10").to_return(body: [].to_json)
         Record.where(color: 'blue').page(3).per(10).first
         Record.where(color: 'blue').per(10).page(3).first
         Record.where(color: 'blue').per(20).page(5).per(10).page(3).first
+        expect(request).to have_been_made.times(3)
       end
     end
 
@@ -64,12 +71,14 @@ describe LHS::Record do
       end
 
       it 'allows to chain pagination methods' do
-        stub_request(:get, "http://local.ch/records?color=blue&page=3&limit=100").to_return(body: [].to_json)
+        request = stub_request(:get, "http://local.ch/records?color=blue&page=3&limit=100").to_return(body: [].to_json)
         Record.where(color: 'blue').page(3).first
-        stub_request(:get, "http://local.ch/records?color=blue&page=3&limit=10").to_return(body: [].to_json)
+        expect(request).to have_been_made.times(1)
+        request = stub_request(:get, "http://local.ch/records?color=blue&page=3&limit=10").to_return(body: [].to_json)
         Record.where(color: 'blue').page(3).per(10).first
         Record.where(color: 'blue').per(10).page(3).first
         Record.where(color: 'blue').per(20).page(5).per(10).page(3).first
+        expect(request).to have_been_made.times(3)
       end
     end
   end
