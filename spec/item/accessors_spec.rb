a3559cc1452075ed7ff9df4eba2c2c20ad9dab68
Cédric Wider <wider.cedric@gmail.com>
2016-07-14 14:42:12 +0200

    Ruby Style Additional Data Accessors (#139)
    * ~ allow snake-style access to data delivered in camel case.
    
    * rubocop

---
+++ spec/item/accessors_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe LHS::Item do
+  context 'accessors' do
+    before(:each) do
+      class Record < LHS::Record
+        endpoint 'http://datastore/records'
+      end
+    end
+
+    it 'accesses camel case keys using underscore syntax' do
+      stub_request(:get, "http://datastore/records?id=1")
+        .to_return(body: { 'AttributeValue' => 42 }.to_json)
+
+      record = Record.find(1)
+      expect(record.attribute_value).not_to be nil
+    end
+  end
+end
