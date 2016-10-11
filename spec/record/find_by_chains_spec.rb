b54022b3577a36607c0950da8ba297e06b35a872
spape <s@pape.io>
2016-10-11 16:03:08 +0200

    + missing find_by! to chains

---
+++ spec/record/find_by_chains_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe LHS::Record do
+  before(:each) do
+    class Record < LHS::Record
+      endpoint 'http://datastore/records/'
+    end
+    stub_request(:get, "http://datastore/records/?limit=1&name=Steve")
+      .to_return(body: [{ name: 'Steve', color: 'blue' }].to_json)
+  end
+
+  it 'allows chaining find_by' do
+    Record.options(params: { color: 'blue' }).find_by(name: 'Steve')
+  end
+
+  it 'allows chaining find_by!' do
+    Record.options(params: { color: 'blue' }).find_by!(name: 'Steve')
+  end
+end
