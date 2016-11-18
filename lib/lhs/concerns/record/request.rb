5b081e93b76e525ef1497d8d4639867919d5e369
Cedric Wider <wider.cedric@gmail.com>
2016-11-18 14:23:49 +0100

    prevent no such method error in case item is nil

--- lib/lhs/concerns/record/request.rb
+++ lib/lhs/concerns/record/request.rb
@@ -52,7 +52,7 @@ class LHS::Record
       def extend_base_collection!(data, addition, key)
         data.each_with_index do |item, i|
           item = item[i] if item.is_a? LHS::Collection
-          link = item[key.to_sym]
+          link = item[key.to_sym] if item.present?
           link.merge_raw!(addition[i]) if link.present?
         end
       end
@@ -86,7 +86,7 @@ class LHS::Record
           target._raw[items_key] = addition.map(&:_raw)
         else
           target._raw[items_key].each_with_index do |item, index|
-            item.merge!(addition[index])
+            item.merge!(addition[index]) if addition[index].present?
           end
         end
       end
