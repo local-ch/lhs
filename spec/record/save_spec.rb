e27cace4a4830ac64c527c05853ffe98a5a515c0
Sebastian Pape <spape@users.noreply.github.com>
2016-11-29 07:45:52 +0100

    ! save endpoint with compiled params (#174)

--- spec/record/save_spec.rb
+++ spec/record/save_spec.rb
@@ -14,14 +14,24 @@ describe LHS::Record do
       let(:campaign_id) { 12345 }
       let(:object) { { recommended: true } }
       let(:item) { Feedback.new(object.merge(campaign_id: campaign_id)) }
-
-      it 'removes params used to compute url from send data' do
-        datastore_request = stub_request(:post, "#{datastore}/content-ads/#{campaign_id}/feedbacks")
+      let!(:request) do
+        stub_request(:post, "#{datastore}/content-ads/#{campaign_id}/feedbacks")
           .with(body: object.to_json)
           .to_return(status: 200, body: object.to_json)
+      end
 
+      it 'removes params used to compute url from send data' do
         item.save!
-        expect(datastore_request).to have_been_made.once
+        expect(request).to have_been_made.once
+      end
+
+      context 'item without data' do
+        let(:item) { Feedback.new(object) }
+
+        it 'takes params to find endpoint also from the options' do
+          item.save!(params: { campaign_id: campaign_id })
+          expect(request).to have_been_made.once
+        end
       end
     end
   end
