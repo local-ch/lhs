21f658cb564768729bba1d44189d2cc28c6199df
Sebastian Pape <spape@users.noreply.github.com>
2016-12-21 15:39:28 +0100

    Load and merge created data through location header (#183)
    * ! load and merge created data through location header
    
    * ! please rubocop
    
    * ~ using tap based on feedback
    
    * ! please rubocop

--- lib/lhs/concerns/item/save.rb
+++ lib/lhs/concerns/item/save.rb
@@ -15,29 +15,34 @@ class LHS::Item < LHS::Proxy
     def save!(options = {})
       options = options.present? ? options.dup : {}
       data = _data._raw.dup
-      if href.present?
-        url = href
-      else
-        endpoint = endpoint_for_persistance(data, options)
-        url = url_for_persistance(endpoint, data, options)
-        endpoint.remove_interpolated_params!(data)
-        endpoint.remove_interpolated_params!(options.fetch(:params, {}))
-        options.merge!(endpoint.options.merge(options)) if endpoint.options
-      end
-
-      options = options.merge(method: :post, url: url, body: data.to_json)
-      options[:headers] ||= {}
-      options[:headers].merge!('Content-Type' => 'application/json')
-
-      data = record_for_persistance.request(options)
-      _data.merge_raw!(data)
-      true
+      url = url_for_persistance!(options, data)
+      create_and_merge_data!(
+        apply_default_creation_options(options, url, data)
+      )
     rescue LHC::Error => e
       self.errors = LHS::Errors.new(e.response)
       raise e
     end
 
     private
+
+    def apply_default_creation_options(options, url, data)
+      options = options.merge(method: :post, url: url, body: data.to_json)
+      options[:headers] ||= {}
+      options[:headers].merge!('Content-Type' => 'application/json')
+      options
+    end
+
+    def create_and_merge_data!(options)
+      direct_response_data = record_for_persistance.request(options)
+      _data.merge_raw!(direct_response_data)
+      response_headers = direct_response_data._request.response.headers
+      if response_headers && response_headers['Location']
+        location_data = record_for_persistance.request(options.merge(url: response_headers['Location'], method: :get, body: nil))
+        _data.merge_raw!(location_data)
+      end
+      true
+    end
 
     def endpoint_for_persistance(data, options)
       record_for_persistance
@@ -56,10 +61,16 @@ class LHS::Item < LHS::Proxy
       _data.class
     end
 
-    def url_for_persistance(endpoint, data, options)
+    def url_for_persistance!(options, data)
+      return href if href.present?
+      endpoint = endpoint_for_persistance(data, options)
       endpoint.compile(
         merge_data_with_options(data, options)
-      )
+      ).tap do
+        endpoint.remove_interpolated_params!(data)
+        endpoint.remove_interpolated_params!(options.fetch(:params, {}))
+        options.merge!(endpoint.options.merge(options)) if endpoint.options
+      end
     end
   end
 end
