0f441c2cf623432debb626c7e3ba2abc575369fb
Flavio Pellanda <flavio.pellanda@gmx.ch>
2017-01-31 11:27:52 +0100

    Add proper autoloading with middleware fixes #185 (#186)

--- lib/lhs.rb
+++ lib/lhs.rb
@@ -1,6 +1,18 @@
 require 'lhc'
 
 module LHS
+  class RequireLhsRecords
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      Dir.glob(Rails.root.join('app/models/**/*.rb')).each do |file|
+        require_dependency file if File.read(file).match('LHS::Record')
+      end
+      @app.call(env)
+    end
+  end
 end
 
 Gem.find_files('lhs/**/*.rb').sort.each { |path| require path }
@@ -8,8 +20,8 @@ Gem.find_files('lhs/**/*.rb').sort.each { |path| require path }
 # Preload all the LHS::Records that are defined in app/models
 class Engine < Rails::Engine
   initializer 'Load all LHS::Records from app/models/**' do |app|
-    Dir.glob(app.root.join('app/models/**/*.rb')).each do |file|
-      require file if File.read(file).match('LHS::Record')
-    end
+    next if app.config.cache_classes
+
+    app.config.middleware.use LHS::RequireLhsRecords
   end
 end
