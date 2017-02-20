688e8012b034cd6e3d4a360436932c61b5408a0d
Cedric Wider <wider.cedric@gmail.com>
2017-02-20 14:23:54 +0100

    remove even more duplication

--- spec/record/includes_spec.rb
+++ spec/record/includes_spec.rb
@@ -480,14 +480,10 @@ describe LHS::Record do
         .to_return(body:{
           category_relations: [{
             "href": "http://datastore/category/1"
-          }, {
-            "href": "http://datastore/category/2"
           }]
         }.to_json)
       stub_request(:get, "http://datastore/category/1")
         .to_return(body: { name: 'Food' }.to_json)
-      stub_request(:get, "http://datastore/category/2")
-        .to_return(body: { name: 'Drinks' }.to_json)
     end
 
     it 'includes and merges linked resources in case of an array of links' do
