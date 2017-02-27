571540ef4fb5eca9dfe36fdc526cccf36bfb0ac0
Krzysztof Sakwerda <ksakwerda@gmail.com>
2017-02-27 16:58:44 +0100

    ! always load LHS classes once

--- lib/lhs.rb
+++ lib/lhs.rb
@@ -7,10 +7,14 @@ module LHS
     end
 
     def call(env)
+      self.class.require_records
+      @app.call(env)
+    end
+
+    def self.require_records
       Dir.glob(Rails.root.join('app/models/**/*.rb')).each do |file|
         require_dependency file if File.read(file).match('LHS::Record')
       end
-      @app.call(env)
     end
   end
 end
@@ -20,6 +24,7 @@ Gem.find_files('lhs/**/*.rb').sort.each { |path| require path }
 # Preload all the LHS::Records that are defined in app/models
 class Engine < Rails::Engine
   initializer 'Load all LHS::Records from app/models/**' do |app|
+    LHS::RequireLhsRecords.require_records
     next if app.config.cache_classes
 
     app.config.middleware.use LHS::RequireLhsRecords
