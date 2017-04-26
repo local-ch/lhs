4c01bec692c981955a378c2088c8d5925d8a0ba5
spape <s@pape.io>
2017-04-26 09:37:11 +0200

    - logging for debugging reasons

--- lib/lhs/railtie.rb
+++ lib/lhs/railtie.rb
@@ -9,7 +9,6 @@ module LHS
         def prepare_lhs_request_cycle_cache
           return unless LHS.config.request_cycle_cache_enabled
           LHS::Record::RequestCycleCache::RequestCycleThreadRegistry.request_id = [Time.now.to_f, request.object_id].join('#')
-          Rails.logger.error "REQUEST ID #{LHS::Record::RequestCycleCache::RequestCycleThreadRegistry.request_id}"
         end
       end
     end
