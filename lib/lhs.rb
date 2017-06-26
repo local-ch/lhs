adc132cc61d017a69d8253fccf4383d09e67776b
spape <s@pape.io>
2017-06-26 15:38:18 +0200

    ~ get autoloading started

--- lib/lhs.rb
+++ lib/lhs.rb
@@ -1,40 +1,30 @@
 require 'lhc'
-Dir[File.dirname(__FILE__) + '/lhs/concerns/lhs/*.rb'].sort.each { |file| require file }
 
 module LHS
+  autoload :Configuration,      'lhs/concerns/configuration'
+  autoload :AutoloadRecords,    'lhs/concerns/autoload_records'
+  autoload :Collection,         'lhs/collection'
+  autoload :Complex,            'lhs/complex'
+  autoload :Config,             'lhs/config'
+  autoload :Data,               'lhs/data'
+  autoload :Endpoint,           'lhs/endpoint'
+  autoload :Errors,             'lhs/errors/base'
+  module Errors
+    autoload :Nested,           'lhs/errors/nested'
+  end
+  autoload :Inspect,            'lhs/concerns/inspect'
+  autoload :Item,               'lhs/item'
+  autoload :Pagination,         'lhs/pagination/base'
+  module Pagination
+    autoload :Offset,           'lhs/pagination/offset'
+    autoload :Page,             'lhs/pagination/page'
+    autoload :Start,            'lhs/pagination/start'
+  end
+  autoload :Proxy,              'lhs/proxy'
+  autoload :Record,             'lhs/record'
+
   include Configuration
-  class RequireLhsRecords
-    def initialize(app)
-      @app = app
-    end
+  include AutoloadRecords
 
-    def call(env)
-      self.class.require_records
-      @app.call(env)
-    end
-
-    def self.require_records
-      Dir.glob(Rails.root.join('app/models/**/*.rb')).each do |file|
-        require_dependency file if File.read(file).match('LHS::Record')
-      end
-    end
-  end
-end
-
-Gem.find_files('lhs/**/*.rb')
-  .sort
-  .reject do |path|
-    (!defined?(Rails) && File.basename(path).include?('railtie.rb')) # don't require railtie if Rails is not around
-  end.each do |path|
-    require path
-  end
-
-# Preload all the LHS::Records that are defined in app/models
-class Engine < Rails::Engine
-  initializer 'Load all LHS::Records from app/models/**' do |app|
-    LHS::RequireLhsRecords.require_records
-    next if app.config.cache_classes
-
-    app.config.middleware.use LHS::RequireLhsRecords
-  end
+  require 'lhs/railtie' if defined?(Rails)
 end
---
+++ lib/lhs/concerns/autoload_records.rb
@@ -0,0 +1,37 @@
+require 'active_support'
+
+# Preload all the LHS::Records that are defined in app/models/*
+# in order to collect record endpoints and to be able to identify records from hrefs
+# and not only from model constant names (which is different to ActiveRecord)
+module AutoloadRecords
+  extend ActiveSupport::Concern
+
+  included do
+    class Engine < Rails::Engine
+      initializer 'Load all LHS::Records from app/models/**' do |app|
+        Middleware.require_records
+        next if app.config.cache_classes
+
+        app.config.middleware.use Middleware
+      end
+
+      class Middleware
+        def initialize(app)
+          @app = app
+        end
+
+        def call(env)
+          self.class.require_records
+          @app.call(env)
+        end
+
+        def self.require_records
+          Dir.glob(Rails.root.join('app/models/**/*.rb')).each do |file|
+            binding.pry
+            require_dependency file if File.read(file).match('LHS::Record')
+          end
+        end
+      end
+    end
+  end
+end
