6c84e313007f8a9e33f20747d865854395c8daa7
Sebastian Pape <spape@users.noreply.github.com>
2017-07-26 09:24:44 +0200

    ! dig (#234)

---
+++ spec/item/dig_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe LHS::Item do
+  before(:each) do
+    class Record < LHS::Record
+      endpoint ':datastore/records'
+    end
+  end
+
+  let(:json) do
+    {
+      local_entry: {
+        local_entry_id: 'ABC123'
+      }
+    }
+  end
+
+  let(:item) do
+    LHS::Data.new(json, nil, Record)
+  end
+
+  it 'is possible to dig data' do
+    expect(
+      item.dig(:local_entry, :local_entry_id)
+    ).to eq 'ABC123'
+  end
+end
