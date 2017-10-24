8c60bab4987e24d487ca97a65dac97bdeca9ae6a
spape <s@pape.io>
2017-10-24 12:35:43 +0200

    ! parent root fix

--- spec/record/has_many_spec.rb
+++ spec/record/has_many_spec.rb
@@ -20,15 +20,23 @@ describe LHS::Record do
     let(:location) { Location.find(1) }
     let(:listing) { location.listings.first }
 
-    it 'casts the relation into the correct type' do
+    before(:each) do
       stub_request(:get, 'http://uberall/locations/1')
         .to_return(body: {
           listings: [{
             directory: { name: 'Instagram' }
           }]
         }.to_json)
+    end
+
+    it 'casts the relation into the correct type' do
       expect(listing).to be_kind_of(Listing)
       expect(listing.supported?).to eq true
+    end
+
+    it 'keeps hirachy when casting it to another class on access' do
+      expect(listing._root._raw).to eq location._raw
+      expect(listing.parent.parent._raw).to eq location._raw
     end
   end
 end
