93efa404bb4543a22b0ebfa85f4fd99a2b4d5b35
spape <s@pape.io>
2018-01-04 15:03:01 +0100

    + readme for relation has_many options

--- spec/record/has_many_spec.rb
+++ spec/record/has_many_spec.rb
@@ -1,36 +1,73 @@
 require 'rails_helper'
 
 describe LHS::Record do
+
+  let(:listing) { location.listings.first }
+
   before(:each) do
-    class Location < LHS::Record
-      endpoint 'http://uberall/locations'
-      endpoint 'http://uberall/locations/:id'
-      has_many :listings
-    end
-
-    class Listing < LHS::Record
-
-      def supported?
-        true
-      end
-    end
+    stub_request(:get, 'http://uberall/locations/1')
+      .to_return(body: {
+        listings: [{
+          directory: { name: 'Instagram' }
+        }]
+      }.to_json)
   end
 
   context 'has_many' do
-    let(:location) { Location.find(1) }
-    let(:listing) { location.listings.first }
 
     before(:each) do
-      stub_request(:get, 'http://uberall/locations/1')
-        .to_return(body: {
-          listings: [{
-            directory: { name: 'Instagram' }
-          }]
-        }.to_json)
+      class Location < LHS::Record
+        endpoint 'http://uberall/locations'
+        endpoint 'http://uberall/locations/:id'
+        has_many :listings
+      end
+
+      class Listing < LHS::Record
+
+        def supported?
+          true
+        end
+      end
     end
+
+    let(:location) { Location.find(1) }
 
     it 'casts the relation into the correct type' do
       expect(listing).to be_kind_of(Listing)
+      expect(listing.supported?).to eq true
+    end
+
+    it 'keeps hirachy when casting it to another class on access' do
+      expect(listing._root._raw).to eq location._raw
+      expect(listing.parent.parent._raw).to eq location._raw
+    end
+  end
+
+  context 'custom class_name' do
+
+    before(:each) do
+      module Uberall
+        class Location < LHS::Record
+          endpoint 'http://uberall/locations'
+          endpoint 'http://uberall/locations/:id'
+          has_many :listings, class_name: 'Uberall::Listing'
+        end
+      end
+
+      module Uberall
+        class Listing < LHS::Record
+
+          def supported?
+            true
+          end
+        end
+      end
+    end
+
+    let(:location) { Uberall::Location.find(1) }
+
+    it 'casts the relation into the correct type' do
+      expect(listing).to be_kind_of(Uberall::Listing)
       expect(listing.supported?).to eq true
     end
 
