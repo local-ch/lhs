8a2d67ea5542af44cec5b04b5d338f372cffe576
spape <s@pape.io>
2018-06-13 15:57:11 +0200

    ! fix accessing options

--- lib/lhs/version.rb
+++ lib/lhs/version.rb
@@ -1,3 +1,3 @@
 module LHS
-  VERSION = '15.3.2'
+  VERSION = '15.3.3.pre.fixoptions.1'
 end
---
+++ spec/record/options_getter_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe LHS::Record do
+  
+  context 'options' do
+    before do
+      class Record < LHS::Record
+        endpoint 'http://local.ch/records'
+      end
+    end
+
+    let(:raw) do
+      { options: { criticality: :high } }
+    end
+
+    def record
+      LHS::Record.new LHS::Data.new(raw, nil, Record)
+    end
+
+    it 'is possible to fetch data from a key called options from an instance' do
+      expect(record.options.criticality).to eq :high
+    end
+  end
+end
