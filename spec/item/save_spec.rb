7aaa0466b00287f314fd00c99d29b1b2fd6f50c8
Christian Arevalo <contact@christianarevalo.ch>
2019-02-21 11:12:46 +0100

    Undo autoformatting

--- spec/item/save_spec.rb
+++ spec/item/save_spec.rb
@@ -102,11 +102,11 @@ describe LHS::Item do
     it 'keeps header psassed in the options' do
       headers = { 'Stats' => 'first-access' }
       request = stub_request(:post, item.href)
-                  .with(
-                    body: item._raw.to_json,
-                    headers: headers
-                  )
-                  .to_return(status: 200, body: item._raw.to_json)
+        .with(
+          body: item._raw.to_json,
+          headers: headers
+        )
+        .to_return(status: 200, body: item._raw.to_json)
 
       item.save!(headers: headers)
       expect(request).to have_been_requested
