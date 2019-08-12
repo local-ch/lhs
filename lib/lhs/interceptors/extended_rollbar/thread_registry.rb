ddd7153c22f4c7b0d716c42e78df058d342291ac
spape <s@pape.io>
2019-08-12 11:49:41 +0200

    typo

--- lib/lhs/interceptors/extended_rollbar/thread_registry.rb
+++ lib/lhs/interceptors/extended_rollbar/thread_registry.rb
@@ -9,7 +9,7 @@ module LHS
 
       class ThreadRegistry
         # Using ActiveSupports PerThreadRegistry to be able to support Active Support v4.
-        # Will switch to thread_mattr_accessor (which comes with Activesupport) when we dropping support for Active Support v5.
+        # Will switch to thread_mattr_accessor (which comes with Activesupport) when we dropping support for Active Support v4.
         extend ActiveSupport::PerThreadRegistry
         attr_accessor :log
       end
--- lib/lhs/interceptors/request_cycle_cache/thread_registry.rb
+++ lib/lhs/interceptors/request_cycle_cache/thread_registry.rb
@@ -8,7 +8,7 @@ module LHS
       extend ActiveSupport::Concern
       class ThreadRegistry
         # Using ActiveSupports PerThreadRegistry to be able to support Active Support v4.
-        # Will switch to thread_mattr_accessor (which comes with Activesupport) when we dropping support for Active Support v5.
+        # Will switch to thread_mattr_accessor (which comes with Activesupport) when we dropping support for Active Support v4.
         extend ActiveSupport::PerThreadRegistry
         attr_accessor :request_id
       end
