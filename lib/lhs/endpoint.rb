f3316fafd65540b90df8828b2b758b105cd7e4e7
Sebastian Pape <spape@users.noreply.github.com>
2020-02-19 13:18:15 +0100

    Fix cant add new key during iteration (#374)
    * fix "cant add new key during iteration"
    
    * new pre release version
    
    * new pre release version name
    
    * dup when performing iterations (thread safety)
    
    * patch release

--- lib/lhs/endpoint.rb
+++ lib/lhs/endpoint.rb
@@ -4,7 +4,7 @@
 class LHS::Endpoint
 
   def self.for_url(url)
-    template, record = LHS::Record::Endpoints.all.detect do |template, _record|
+    template, record = LHS::Record::Endpoints.all.dup.detect do |template, _record|
       LHC::Endpoint.match?(url, template)
     end
     record&.endpoints&.detect { |endpoint| endpoint.url == template }
--- lib/lhs/version.rb
+++ lib/lhs/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module LHS
-  VERSION = '21.1.0'
+  VERSION = '21.1.2'
 end
