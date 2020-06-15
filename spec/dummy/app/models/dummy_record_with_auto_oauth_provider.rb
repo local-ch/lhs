118c5710be86253019e2026b1d485133ceba65a2
Sebastian Pape <s@pape.io>
2020-06-15 16:48:38 +0200

    fix rubocop

--- spec/dummy/app/models/dummy_record_with_auto_oauth_provider.rb
+++ spec/dummy/app/models/dummy_record_with_auto_oauth_provider.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DummyRecordWithAutoOauthProvider < Providers::InternalServices
   endpoint 'http://internalservice/v2/records'
   endpoint 'http://internalservice/v2/records/{id}'
