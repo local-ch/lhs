e326c450a8fa84cfe56e3633ed437e26b2bf5e90
dimameshcharakou <dzmitry.meshcharakou@localsearch.ch>
2022-05-17 17:40:17 +0200

    Fix expand of data for collections.

--- spec/record/includes_after_expansion_spec.rb
+++ spec/record/includes_after_expansion_spec.rb
@@ -69,4 +69,51 @@ describe LHS::Record do
       ).to eq 'OPL'
     end
   end
+
+  context 'with collections' do
+    before do
+      class User < LHS::Record
+        endpoint 'http://users'
+        endpoint 'http://users/{id}'
+      end
+
+      class Places < LHS::Record
+        endpoint 'http://users/{id}/places'
+        endpoint 'http://places/{id}'
+      end
+
+      stub_request(:get, 'http://users?email=user@example.com')
+        .to_return(
+          body: {
+            href: 'http://users?email=user@example.com',
+            items: [
+              {
+                places: {
+                  href: 'http://users/1/places'
+                }
+              }
+            ]
+          }.to_json
+        )
+
+      stub_request(:get, 'http://users/1/places?limit=100')
+        .to_return(
+          body: {
+            items: [
+              {
+                title: 'Place'
+              }
+            ],
+            total: 0,
+            offset: 0,
+            limit: 10
+          }.to_json
+        )
+    end
+
+    it 'includes resources after expanding plain links' do
+      user = User.includes(:places).find(email: 'user@example.com')
+      expect(user.places.first.title).to eq 'Place'
+    end
+  end
 end
